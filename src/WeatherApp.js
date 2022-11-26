827c87b11ea695dd8b6c4bf4e9b1e5359e763092
Hugo <hugoxu11@gmail.com>
2022-11-26 11:42:47 -0500

    second commit

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,26 +2,19 @@ import './index.css';
 import './WeatherApp.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import * as Weather from 'react-icons/wi';
 import weatherIcons from './weatherIcons/weatherIcons.js';
 import weatherTerms from './weatherIcons/weatherTerms.js';
 import Header from './components/Header/Header.js';
 import SearchLocation from './components/SearchLocation/SearchLocation.js';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather.js';
-// import DailyForecast from './components/DailyForecast/DailyForecast.js';
 import ThreeHourForecast from './components/ThreeHourForecast/ThreeHourForecast.js';
 
 function WeatherApp() {
   const [location, setLocation] = useState('');
   const [currentWeather, setCurrentWeather] = useState({});
   const [hourlyForecast, setHourlyForecast] = useState([]);
-  /*const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);*/
-  // const [dailyForecast, setDailyForecast] = useState([]);
 
   useEffect(() => {
-    // const dailyArr = [];
-    // const threeHourArr = [];
     let latitude, longitude;
     const objIcon = "icon";
     const objAlt = "alt";
@@ -38,29 +31,9 @@ function WeatherApp() {
     const mainWeather = "main_weather";
 
     if (location !== '') {
-      /*axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${location}&days=8&key=3dedb3daa6084a5d97f316adc95e0f6d`)
-        .then((response) => {
-          // console.log(response.data);
-          for (let i = 0; i < response.data.data.length; i++) {
-            let obj = {};
-            let description = response.data.data[i].weather.description.toLowerCase();
-            let icon = response.data.data[i].weather.icon;
-            // console.log(description);
-            // console.log(icon);
-            obj[objIcon] = weatherIcons[description][icon];
-            obj[objAlt] = description;
-            dailyArr.push(obj);
-          }
-          setDailyForecast([...dailyArr]);
-        })
-        .catch((error) => {
-          console.error(error);
-        });*/
 
       axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&cnt=8&appid=6e1a465582c0f73603c073b90273f31f`)
         .then((response) => {
-          console.log(response.data);
-
           const threeHourArr = response.data.list.map((item) => {
               let obj = {};
               let dateTime = item.dt_txt;
@@ -99,7 +72,6 @@ function WeatherApp() {
               currentWeatherObj[wind] = windSpeed;
               currentWeatherObj[sunset] = sunSet;
               currentWeatherObj[sunrise] = sunRise;
-              console.log(currentWeatherObj);
               setCurrentWeather(currentWeatherObj);
             })
             .catch((err) => {
@@ -127,7 +99,6 @@ function WeatherApp() {
           <h2 className="location-title">{location.replace(/,/g, ", ")}</h2>
           <CurrentWeather currentWeather={currentWeather}/>
           <ThreeHourForecast hourArr={hourlyForecast} />
-          {/* <DailyForecast dailyArr={dailyForecast} /> */}
         </section>
       }
     </main>
